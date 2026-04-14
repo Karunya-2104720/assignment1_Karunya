c2abb0f8088b080f715a17f8caa5a2f3ff1d5e51
Karunya-2104720 <chinmayeeallu@gmail.com>
2026-04-14 23:51:15 +0530

    Assignment 1 - Data preprocessing

---
+++ gamescore.py
@@ -0,0 +1,8 @@
+name = input("Enter the player's name: ")
+games_played = int(input("Enter the number of games played: "))
+total_score = int(input("Enter the total score achieved: "))
+average_score = total_score / games_played
+print("\nPlayer:", name)
+print("\nGames Played:", games_played)
+print("\nTotal Score:", total_score)
+print("\nAverage Score:", average_score)
